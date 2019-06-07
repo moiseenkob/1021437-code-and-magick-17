c9baca6ea27a352f432773be10f276b47bded887
Bogdan <moiseenkob@gmail.com>
2019-06-07 18:55:19 +0300

    Внес правки с облаком и функцией

--- js/stat.js
+++ js/stat.js
@@ -28,30 +28,27 @@ var randomColor = function () {
 
 window.renderStatistics = function (ctx, name, times) {
 
-  renderCloud(ctx, 100, 10, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, 90, 0, '#fff');
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
 
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
   ctx.fillText('Ура вы победили!', 120, 41);
   ctx.fillText('Список результатов:', 120, 59);
 
+  var createBar = function () {
+    name[i] === PLAYER ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = randomColor();
+    ctx.fillRect(((140 + (GAP + BAR_W) * i)), 240, BAR_W, -(BAR_H * times[i]) / maxTime);
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.round(times[i]), (140 + (GAP + BAR_W) * i), 270 - (BAR_H * times[i]) / maxTime - 38);
+    ctx.fillText(name[i], (140 + (GAP + BAR_W) * i), 258);
+
+  };
+
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < name.length; i++) {
-    if (name[i] === PLAYER) {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(((140 + (GAP + BAR_W) * i)), 240, BAR_W, -(BAR_H * times[i]) / maxTime);
-      ctx.fillStyle = '#000';
-      ctx.fillText(Math.round(times[i]), (140 + (GAP + BAR_W) * i), 270 - (BAR_H * times[i]) / maxTime - 38);
-      ctx.fillText(name[i], (140 + (GAP + BAR_W) * i), 258);
-    } else {
-      ctx.fillStyle = randomColor();
-      ctx.fillRect(((140 + (GAP + BAR_W) * i)), 240, BAR_W, -(BAR_H * times[i]) / maxTime);
-      ctx.fillStyle = '#000';
-      ctx.fillText(Math.round(times[i]), (140 + (GAP + BAR_W) * i), 270 - (BAR_H * times[i]) / maxTime - 38);
-      ctx.fillText(name[i], (140 + (GAP + BAR_W) * i), 258);
-    }
+    createBar();
   }
 };
 
