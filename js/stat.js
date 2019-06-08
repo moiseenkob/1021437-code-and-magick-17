607d20da008925622ef978be756e22e1fb2b8231
Bogdan <moiseenkob@gmail.com>
2019-06-08 13:10:48 +0300

    Добавил констант, внес параметр в функцию, добавил комментарии

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,20 @@
 'use strict';
+
+/* Константы */
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var BAR_W = 40;
 var BAR_H = 150;
 var GAP = 50;
 var PLAYER = 'Вы';
+var INDENT = 140;
+var DEFAULT_COLOR = '#000';
+var Y_RECTANGLE = 240;
+var Y_TEXT = 232;
+var Y_NAME = 258;
+var TEXT_TITLE_Y = 120;
 
+/* Функция создания внешних объектов (облаков, теней) */
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -20,36 +29,38 @@ var getMaxElement = function (arr) {
   }
   return maxElement;
 };
-
+/* Функция генерации цвета для столбиков */
 var randomColor = function () {
   var x = (Math.random(10, 100)).toString(16).slice(-2);
   return '#' + '00538a' + x;
 };
-
+/* Функция обработки всей статистики и вывода её на экран */
 window.renderStatistics = function (ctx, name, times) {
 
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
   ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 120, 41);
-  ctx.fillText('Список результатов:', 120, 59);
-
-  var createBar = function () {
-
-    ctx.fillStyle = (name[i] === PLAYER ? 'rgba(255, 0, 0, 1)' : randomColor());
-    ctx.fillRect(((140 + (GAP + BAR_W) * i)), 240, BAR_W, -(BAR_H * times[i]) / maxTime);
-    ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), (140 + (GAP + BAR_W) * i), 270 - (BAR_H * times[i]) / maxTime - 38);
-    ctx.fillText(name[i], (140 + (GAP + BAR_W) * i), 258);
-
-  };
+  ctx.fillStyle = DEFAULT_COLOR;
+  ctx.fillText('Ура вы победили!', TEXT_TITLE_Y, 41);
+  ctx.fillText('Список результатов:', TEXT_TITLE_Y, 59);
 
   var maxTime = getMaxElement(times);
 
+  /* Функция создания столбика с рандомным цветом  */
+  var createBar = function (val) {
+
+    ctx.fillStyle = (name[val] === PLAYER ? 'rgba(255, 0, 0, 1)' : randomColor());
+    ctx.fillRect(((INDENT + (GAP + BAR_W) * val)), Y_RECTANGLE, BAR_W, -(BAR_H * times[val]) / maxTime);
+    ctx.fillStyle = DEFAULT_COLOR;
+    ctx.fillText(Math.round(times[val]), (INDENT + (GAP + BAR_W) * val), Y_TEXT - (BAR_H * times[val]) / maxTime);
+    ctx.fillText(name[val], (INDENT + (GAP + BAR_W) * val), Y_NAME);
+
+  };
+
+  /* Цикл с помошью которого пробегаемся по массиву и строем столбики с помощью функции  */
   for (var i = 0; i < name.length; i++) {
-    createBar();
+    createBar(i);
   }
 };
 
